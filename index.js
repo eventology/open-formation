4db99c95572eda1351b18e58d267b56f1035f90b
Chance Hudson <jchancehud@gmail.com>
2018-07-05 22:25:10 -0500

    Remove Promise log polyfill

--- index.js
+++ index.js
@@ -3,23 +3,6 @@
 const _ = require('lodash');
 
 /**
- * Promise polyfill that allows logging
- *
- * The promise returned will always resolve with the value of the previous
- * promise. This allow arbitrary logic/returns in fn.
- *
- * @param {Function} fn The callback function, defaults to console.log
- * @return {Promise} Returns a promise resolving to the previous value (not fn)
- **/
-Promise.prototype.log = Promise.prototype.log || function (fn) {
-  return this.then(arg => {
-    if (fn) fn.call(this, arg);
-    else console.log(arg);
-    return arg;
-  });
-};
-
-/**
  * A serial queue implementation for promises based on the lodash map function
  *
  * @param {Object|Array} objects The contents to iterate over
